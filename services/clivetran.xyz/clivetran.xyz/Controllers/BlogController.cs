82bc3bca4c3642e45ebb30b745993c29bb201e75
Clive Tran <tranvinhnhan@outlook.com>
2022-04-09 09:41:12 +0700

    Init

---
+++ services/clivetran.xyz/clivetran.xyz/Controllers/BlogController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace clivetran.xyz.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        [HttpGet]
+        public string Test()
+        {
+            return "Hello world.";
+        }
+    }
+}
