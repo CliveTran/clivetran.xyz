b82b4df159a61bcbaa51d96559d2adf37c6a9f41
Clive Tran <tranvinhnhan@outlook.com>
2022-04-13 23:55:47 +0700

    Init webapp

---
+++ webapp/clivetran.xyz/src/components/Header.js
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+        <nav className="nav">
+            <code>clivetran.xyz</code>
+            <ul className="nav-menu">
+                <li>Pricing</li>
+                <li>Contact</li>
+                <li>About</li>
+            </ul>
+        </nav>
+    )
+}
---
+++ webapp/clivetran.xyz/src/components/MainContent.js
@@ -0,0 +1,11 @@
+export default function MainContent()
+{
+    return (
+        <ol>
+            <li>Thing #1</li>
+            <li>Thing #2</li>
+            <li>Thing #3</li>
+            <li>Thing #4</li>
+        </ol>
+    )
+}
