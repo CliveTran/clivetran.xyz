dcf9404e52178f7e6c400c568e24d4cda2410a9e
Clive Tran <tranvinhnhan@outlook.com>
2022-04-24 17:07:45 +0700

    Remove backend

--- services/Blog/Api/Dtos/CreatePostDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Api.Dtos
-{
-    public class CreatePostDto
-    {
-        public string Title { get; set; } = string.Empty;
-        public string Content { get; set; } = string.Empty;
-        public string Summary { get; set; } = string.Empty;
-    }
-}
--- services/Blog/Domain/AuditableEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain
-{
-    public class AuditableEntity
-    {
-        public DateTimeOffset Created { get; set; }
-
-        public DateTimeOffset? LastModified { get; set; }
-    }
-}
