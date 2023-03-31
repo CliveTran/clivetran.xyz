9479495998a442b21dcef027f65cbf0323b759f8
Clive Tran <tranvinhnhan@outlook.com>
2023-03-31 22:51:49 +0700

    Update resume

--- src/lib/resume.svelte
+++ src/lib/resume.svelte
@@ -1,21 +1,35 @@
 <script></script>
 
 <div>
-	<h1>Curriculum Vitae</h1>
-	<h2>Education</h2>
+	<h1 class="h1">Curriculum Vitae</h1>
+	<h3 class="h3">Education</h3>
 	<p>
 		University of Foreign Language and Information Technology - Bachelor of Software Engineering
 		(2016 - 2020)
 	</p>
-	<h2>Experience</h2>
-	<h3>Orient Software</h3>
+	<h3 class="h3">Experience</h3>
+	<h4 class="h4">Orient Software</h4>
 	<b>Software Engineer, August 2020 - present</b>
-	<p>Working on a wealth management plaform</p>
+	<p><i>Netwealth - Investors & Wealth management platform</i></p>
 	<ul>
-		<li>Developing APIs with .NET 6/Azure for external and internal uses</li>
+		<li>Developing Public APIs with .NET/Azure for external and internal uses</li>
 		<li>Initializing, developing, managing CI/CD pipelines, API gateway</li>
 		<li>Identifying defects, providing feedback for improved performance, usability</li>
+		<li>Liaising with stakeholders</li>
 	</ul>
+	<p><i>OPAL 2.0 - Digital learning space for employees and partners of the Ministry of Education, Singapore</i></p>
+	<ul>
+		<li>Developing core modules (course management, learning management)</li>
+		<li>Analyzing, dissecting work items into smaller tasks</li>
+	</ul>
+	<h3 class="h3">Achievements</h3>
+	<p>Microsoft Certified: <a href="https://www.credly.com/badges/da7db76b-c31e-445c-b299-66c8a384fb6c">AZ-900</a></p>
 </div>
 
-<style></style>
+<style lang="postcss">
+	ul {
+		padding-inline-start: 40px;
+		@apply my-2;
+		list-style-type: disc;
+	}
+</style>
