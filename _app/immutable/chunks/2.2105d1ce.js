09fa0ac347cf7a2ce86a8bc58a4a9432a2875547
Clive Tran <tranvinhnhan@outlook.com>
2023-04-01 00:58:56 +0700

    Updates

---
+++ _app/immutable/chunks/2.2105d1ce.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.ddd11cb1.js";export{t as component};
---
+++ _app/immutable/chunks/5.5cde17ae.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/resume-page.svelte.be737d8f.js";export{t as component};
