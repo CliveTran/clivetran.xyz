79effadcc4f1d11d98f697f7c9119c8746108dea
Clive Tran <tranvinhnhan@outlook.com>
2023-03-31 21:51:54 +0700

    Update footer

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -1,39 +1,37 @@
 <script></script>
 
-<div>
-	<div>
-		<div>
-			<div>
-				<h3>Me on social media</h3>
-				<ul>
-					<li>
-						<a href="https://github.com/CliveTran" target="_blank" rel="noreferrer">Github</a>
-					</li>
-					<li>
-						<a href="https://leetcode.com/CliveTran/" target="_blank" rel="noreferrer">Leetcode</a>
-					</li>
-					<li>
-						<a href="https://www.linkedin.com/in/vinhnhan/" target="_blank" rel="noreferrer"
-							>Linkedin</a
-						>
-					</li>
-				</ul>
-			</div>
-			<div>
-				<h3>Things you might like</h3>
-				<ul>
-					<li><a href="/">Book recommendations</a></li>
-					<li>
-						<a href="https://github.com/florinpop17/app-ideas" target="_blank" rel="noreferrer"
-							>App ideas</a
-						>
-					</li>
-					<li><a href="/">Useful websites</a></li>
-				</ul>
-			</div>
+<div class="container mx-auto">
+	<div class="my-5 justify-around">
+		<div class="flex">
+			<h3 class="font-semibold">Me on social media:</h3>
+			<ul class="flex">
+				<li class="mx-2">
+					<a href="https://github.com/CliveTran" target="_blank" rel="noreferrer">Github</a>
+				</li>
+				<li class="mx-2">
+					<a href="https://leetcode.com/CliveTran/" target="_blank" rel="noreferrer">Leetcode</a>
+				</li>
+				<li class="mx-2">
+					<a href="https://www.linkedin.com/in/vinhnhan/" target="_blank" rel="noreferrer"
+						>Linkedin</a
+					>
+				</li>
+			</ul>
 		</div>
-		<p>&#169; 2022 - Clive Tran</p>
+		<div class="flex">
+			<h3 class="font-semibold">Things you might like:</h3>
+			<ul class="flex">
+				<li class="mx-2"><a href="/">Book recommendations</a></li>
+				<li class="mx-2">
+					<a href="https://github.com/florinpop17/app-ideas" target="_blank" rel="noreferrer"
+						>App ideas</a
+					>
+				</li>
+				<li class="mx-2"><a href="/">Useful websites</a></li>
+			</ul>
+		</div>
 	</div>
+	<p>&#169; 2023 - Clive Tran</p>
 </div>
 
 <style></style>
