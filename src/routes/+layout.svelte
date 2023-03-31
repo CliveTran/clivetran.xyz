7366763802fd68315c732232258f3404467ced26
Clive Tran <tranvinhnhan@outlook.com>
2023-04-01 00:51:41 +0700

    Update prerender

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,6 @@
 	import Footer from '../lib/footer.svelte';
 	import Navbar from '../lib/navbar.svelte';
 	import '../app.css';
-
-	export const prerender = true;
 </script>
 
 <div class="flex flex-col h-screen max-sm:p-4">
