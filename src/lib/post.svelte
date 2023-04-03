299604eebaf82f6f22f740c30ab9e930bbaa225d
Clive Tran <tranvinhnhan@outlook.com>
2023-04-03 23:31:57 +0700

    Add buy-me-a-coffee

--- src/lib/post.svelte
+++ src/lib/post.svelte
@@ -10,8 +10,20 @@
 
 <div class="post-container">{@html html}</div>
 
+<div class="buy-me-a-coffee">
+	<a href="https://www.buymeacoffee.com/clivetran"
+		><img
+			alt="Buy me a coffee"
+			src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=clivetran&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff"
+		/></a
+	>
+</div>
+
 <style lang="postcss">
 	.post-container {
 		max-width: 768px;
+	}
+	.buy-me-a-coffee {
+		@apply my-10;
 	}
 </style>
