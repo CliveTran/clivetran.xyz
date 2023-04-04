f54ba8cc917cb7bce36cfad3ee35c7cc159ae636
Clive Tran <tranvinhnhan@outlook.com>
2023-04-04 23:35:06 +0700

    Update footer

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -1,10 +1,10 @@
 <script></script>
 
 <div class="container mx-auto">
-	<div class="my-5 justify-around max-sm:flex">
-		<div class="sm:flex">
+	<div id="web" class="my-5 max-sm:hidden justify-around sm:">
+		<div class="flex">
 			<p class="font-semibold">Me on social media:</p>
-			<ul class="sm:flex">
+			<ul class="flex">
 				<li class="mx-2">
 					<a href="https://github.com/CliveTran" target="_blank" rel="noreferrer">Github</a>
 				</li>
@@ -18,9 +18,9 @@
 				</li>
 			</ul>
 		</div>
-		<div class="sm:flex">
+		<div class="flex">
 			<p class="font-semibold">Things you might like:</p>
-			<ul class="sm:flex">
+			<ul class="flex">
 				<li class="mx-2"><a href="/">Book recommendations</a></li>
 				<li class="mx-2">
 					<a href="https://github.com/florinpop17/app-ideas" target="_blank" rel="noreferrer"
@@ -31,7 +31,42 @@
 			</ul>
 		</div>
 	</div>
+	<div id="mobile" class="my-5 sm:hidden">
+		<hr class="mt-10 mx-auto" />
+		<div class="mt-5">
+			<p class="font-semibold">Me on social media:</p>
+			<ul>
+				<li>
+					<a href="https://github.com/CliveTran" target="_blank" rel="noreferrer">Github</a>
+				</li>
+				<li>
+					<a href="https://leetcode.com/CliveTran/" target="_blank" rel="noreferrer">Leetcode</a>
+				</li>
+				<li>
+					<a href="https://www.linkedin.com/in/vinhnhan/" target="_blank" rel="noreferrer"
+						>Linkedin</a
+					>
+				</li>
+			</ul>
+		</div>
+		<div class="mt-5">
+			<p class="font-semibold">Things you might like:</p>
+			<ul class="">
+				<li><a href="/">Books recommendations</a></li>
+				<li>
+					<a href="https://github.com/florinpop17/app-ideas" target="_blank" rel="noreferrer"
+						>App ideas</a
+					>
+				</li>
+				<li><a href="/">Useful websites</a></li>
+			</ul>
+		</div>
+	</div>
 	<p class="text-center">&#169; 2023 - Clive Tran</p>
 </div>
 
-<style></style>
+<style lang="postcss">
+	div#mobile ul li {
+		@apply my-2;
+	}
+</style>
