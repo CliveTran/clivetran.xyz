d4306e597d839fda5e1c84ef183b4ffb395399d7
Clive Tran <tranvinhnhan@outlook.com>
2023-06-11 02:56:57 +0700

    New post

--- src/routes/blog/data.ts
+++ src/routes/blog/data.ts
@@ -1,4 +1,10 @@
 export const posts = [
+
+    {
+        slug: 'github-repos-that-you-wish-you-had-known-about-earlier',
+        title: 'GitHub repos that you wish you had known about earlier',
+        date: "11 June, 2023"
+    },
     {
         slug: 'useful-tips-that-will-make-your-life-easier-as-a-developer',
         title: 'Useful tips that will make your life easier as a developer',
